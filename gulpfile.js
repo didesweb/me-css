ae639fb997c7da4bc6ea4759c5e9c5fb76a6d88f
Roque Arnas <roquearnas@gmail.com>
2026-05-06 19:20:20 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,136 @@
 /*
-    Workflow sass + browser-sync + uglify + purgecss
+    Workflow moderno: Sass + BrowserSync + Terser + PurgeCSS
     Roque Arnás Izquierdo
     https://didesweb.com/
 */
-var path        = "localhost/jorge-barbera";
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sass        = require('gulp-sass');
-var plumber     = require('gulp-plumber');
-var cleanCSS   = require('gulp-clean-css');
-var uglify      = require('gulp-uglify');
-var purgecss    = require('gulp-purgecss');
 
-gulp.task('browser-sync', function() {
-    var files = ['**/*.php'];
-      browserSync.init(files, {
-      proxy: path,
-      notify: false
+import gulp from "gulp";
+import browserSync from "browser-sync";
+import plumber from "gulp-plumber";
+import cleanCSS from "gulp-clean-css";
+import terser from "gulp-terser";
+import purgecss from "gulp-purgecss";
+import sourcemaps from "gulp-sourcemaps";
+import * as sass from "sass";
+import through from "through2";
+import Vinyl from "vinyl";
+
+const server = browserSync.create();
+
+// -----------------------------------------------------
+// CONFIG
+// -----------------------------------------------------
+const proxyPath = "localhost/me";
+
+const paths = { 
+    styles: { 
+        src: ["assets/styles/**/*.scss", "!assets/styles/**/_*.scss"], 
+        dest: "app/styles" 
+    },
+    scripts: { 
+        src: "assets/scripts/**/*.js", 
+        dest: "app/scripts"
+    }
+};
+
+// -----------------------------------------------------
+// HELPERS
+// -----------------------------------------------------
+
+function compileSassModern(src, dest) {
+    return gulp.src(src)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(
+            through.obj((file, enc, cb) => {
+                try {
+                    const result = sass.compile(file.path, {
+                        style: "expanded",
+                        sourceMap: true
+                    });
+
+                    const cssFile = new Vinyl({
+                        cwd: file.cwd,
+                        base: file.base,
+                        path: file.path.replace(/\.scss$/, ".css"),
+                        contents: Buffer.from(result.css)
+                    });
+
+                    cb(null, cssFile);
+                } catch (err) {
+                    console.error(err);
+                    cb(err);
+                }
+            })
+        )
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(dest))
+        .pipe(server.stream());
+}
+
+// -----------------------------------------------------
+// TASKS
+// -----------------------------------------------------
+
+// BrowserSync
+export function serve() {
+    server.init({
+        proxy: proxyPath,
+        notify: false
     });
-});
 
-gulp.task('purgecss', function () {
-    gulp.src('app/styles/main.css')
-    .pipe(
-      purgecss({
-        content: ['**/*.php']
-      })
-    )
-    .pipe(gulp.dest('app/styles'))
-})
+    gulp.watch("**/*.php").on("change", server.reload);
+}
 
-gulp.task('sass', function () {
-    gulp.src('assets/styles/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('app/styles'))
-    .pipe(reload({stream:true}));
-});
+// Sass global
+export function styles() {
+    return compileSassModern(paths.styles.src, paths.styles.dest);
+}
 
-gulp.task('uglify', function() {
-    gulp.src('assets/scripts/**/*.js')
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(gulp.dest('app/scripts'))
-    .pipe(reload({stream:true}));
-});
+// JS global
+export function scripts() {
+    return gulp.src(paths.scripts.src)
+        .pipe(plumber())
+        .pipe(terser())
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(server.stream());
+}
 
-gulp.task('default', ['sass', 'uglify', 'browser-sync'], function () {
-    gulp.watch("assets/styles/**/*.scss", ['sass']);
-    gulp.watch("assets/scripts/**/*.js", ['uglify']);
-});
+// PurgeCSS (solo producción)
+export function purge() {
+    return gulp.src("app/styles/main.css")
+        .pipe(
+            purgecss({
+                content: ["**/*.php"]
+            })
+        )
+        .pipe(gulp.dest("app/styles"));
+}
 
-gulp.task('production', ['purgecss'], function () {
-    gulp.start('production');
-});
+// -----------------------------------------------------
+// WATCHERS
+// -----------------------------------------------------
+export function watchFiles() {
+    gulp.watch("assets/styles/**/*.scss", styles);
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+// -----------------------------------------------------
+// MODERN TASKS
+// -----------------------------------------------------
+
+// Desarrollo
+export const dev = gulp.series(
+    gulp.parallel(styles, scripts),
+    gulp.parallel(serve, watchFiles)
+);
+
+// Producción
+export const production = gulp.series(
+    gulp.parallel(styles),
+    purge
+);
+
+// Default
+export default dev;
---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>me css</title>
+	<link rel="stylesheet" href="app/styles/main.css">
+</head>
+<body>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-12 ta-c px-15">
+				<div class="">
+					<h1 class="fz-32">me css</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-12 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-12
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-6 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-6
+				</div>
+			</div>
+			<div class="col-6 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-6
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-4 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-4
+				</div>
+			</div>
+			<div class="col-4 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-4
+				</div>
+			</div>
+			<div class="col-4 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-4
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-3 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-3
+				</div>
+			</div>
+			<div class="col-3 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-3
+				</div>
+			</div>
+			<div class="col-3 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-3
+				</div>
+			</div>
+			<div class="col-3 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-3
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-2 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-2
+				</div>
+			</div>
+			<div class="col-2 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-2
+				</div>
+			</div>
+			<div class="col-2 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-2
+				</div>
+			</div>
+			<div class="col-2 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-2
+				</div>
+			</div>
+			<div class="col-2 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-2
+				</div>
+			</div>
+			<div class="col-2 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-2
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+			<div class="col-1 ta-c px-15">
+				<div class="bd-1 bd-black">
+					col-1
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-12 ta-c px-15">
+				<div class="bd-1 bd-black">
+					<span class="fw-100">.fw-100</span>
+					<span class="fw-200">.fw-200</span>
+					<span class="fw-300">.fw-300</span>
+					<span class="fw-400">.fw-400</span>
+					<span class="fw-500">.fw-500</span>
+					<span class="fw-600">.fw-600</span>
+					<span class="fw-700">.fw-700</span>
+					<span class="fw-800">.fw-800</span>
+					<span class="fw-900">.fw-900</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-12 ta-c px-15">
+				<div class="bd-1 bd-black">
+					<span class="fz-8">.fz-8</span>
+					<span class="fz-10">.fz-10</span>
+					<span class="fz-14">.fz-14</span>
+					<span class="fz-20">.fz-20</span>
+					<span class="fz-32">.fz-32</span>
+					<span class="fz-48">.fz-48</span>
+					<span class="fz-56">.fz-56</span>
+					<span class="fz-56">...</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-12 ta-c px-15">
+				<div class="bd-1 bd-black px-15">
+					<div class="ta-l">.ta-l</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-12 ta-c px-15">
+				<div class="bd-1 bd-black px-15">
+					<div class="ta-c">.ta-c</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box mx-a py-30 px-15">
+		<div class="d-f fxw-w m-ng-15">
+			<div class="col-6 ta-c px-15 bg-black py-15">
+				<div class="px-15">
+					<a href="#" class="btn bg-white c-black bd-black">.btn .btn-white</a>
+				</div>
+			</div>
+			<div class="col-6 ta-c px-15 py-15">
+				<div class="px-15">
+					<a href="#" class="btn bg-black c-white bd-black">.btn .btn-black</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
